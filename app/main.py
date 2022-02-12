6a9a861576143b48bc53d555c5ef87129ed767e8
Guilherme Esgario <guilherme.esgario@gmail.com>
2022-02-12 13:19:13 -0300

    Add flask limiter

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ import os
 import cv2
 import numpy as np
 
-from flask import Flask, request, jsonify, make_response, render_template
+from flask import Flask, request, render_template
 from flask_cors import CORS, cross_origin
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 from app.inference import run_inference
 from app.utils import get_base64_image, reshape_image
@@ -11,8 +13,7 @@ from app.utils import get_base64_image, reshape_image
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-app = Flask(__name__)
+limiter = Limiter(app, key_func=get_remote_address)
 
 
 @app.route('/')  # Homepage
@@ -21,6 +22,7 @@ def home():
 
 
 @app.route("/predict", methods=["POST"])
+@limiter.limit("3/minute")
 @cross_origin()
 def predict():
     try:
@@ -46,6 +48,17 @@ def predict():
 
     return render_template('index.html', data=data, error_msg='')  # rendering the predicted result
 
+
+def page_too_many_requests(e):
+    msg = 'Too many requests per minute. Wait a while and try again.'
+    return render_template('index.html', data={}, error_msg=msg)
+
+
+def init():
+    app.register_error_handler(429, page_too_many_requests)
+
+
+init()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
